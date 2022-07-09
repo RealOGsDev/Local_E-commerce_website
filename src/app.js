feb85c22a54b0fa726bc20ae22650bae00156809
Keshab <www.keshab.kk@gmail.com>
2022-07-09 15:07:31 +0530

    added scripts

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const port = process.env.PORT || 3000;
+const app = express();
+const path = require("path");
+
+const staticPath = path.join(__dirname, "../public");
+const templatePath = path.join(__dirname, "../template/views");
+
+app.set("view engine", "hbs");
+app.set("views", templatePath);
+app.use(express.static(staticPath));
+
+app.get("/", (req, res) => {
+  res.render("userlogin");
+});
+
+app.get("/signup", (req, res) => {
+  res.render("usersignup");
+});
+
+app.get("/shops", (req, res) => {
+  res.render("shops");
+});
+
+
+app.listen(port, () => {
+    console.log("listening");
+  });
+  
